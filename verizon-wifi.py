7efcb4944a1d776839b6561330261561dfb5e78b
ferasdour <no_never001@yahoo.com>
2024-12-03 10:43:36 -0600

    Testing change verizon-wifi.py
    trying to see if this will ever hit/hit within 1 week, without sorting verbs and nouns.

--- verizon-wifi.py
+++ verizon-wifi.py
@@ -1,26 +1,26 @@
 import random, time, nltk, argparse
 
-def expand():
-    nltk.download('averaged_perceptron_tagger_eng')
-    for i in open("words.txt").readlines():
-        text = nltk.word_tokenize(str(i))
-        for deets in nltk.pos_tag(text):
-            if "V" in str(deets[1]):
-                if "ing" in i:
-                    #idk why some verbs are only showing up as ing, think i might need to recheck how this is referenced
-                    open('verbs.txt','a').write(str(i).replace('ing',''))
-                    open('verbs.txt','a').write(i)
-            if "N" in str(deets[1]):
-                open('nouns.txt','a').write(i)
+#def expand():
+#    nltk.download('averaged_perceptron_tagger_eng')
+#    for i in open("words.txt").readlines():
+#        text = nltk.word_tokenize(str(i))
+#        for deets in nltk.pos_tag(text):
+#            if "V" in str(deets[1]):
+#                if "ing" in i:
+#                    #idk why some verbs are only showing up as ing, think i might need to recheck how this is referenced
+#                    open('verbs.txt','a').write(str(i).replace('ing',''))
+#                    open('verbs.txt','a').write(i)
+#            if "N" in str(deets[1]):
+#                open('nouns.txt','a').write(i)
 
 def run():
-    nouns=open("nouns.txt").read().splitlines()
-    verbs=open("verbs.txt").read().splitlines()
+    words=open("words.txt").read().splitlines()
+ #   verbs=open("verbs.txt").read().splitlines()
     # generate verizon passwords
     while True:
         # Debugging step:
         # time.sleep(5)
-        guess=str("{}-{}{}-{}".format(random.choice(nouns),random.choice(verbs),random.randrange(0,9),random.choice(nouns)))
+        guess=str("{}-{}{}-{}".format(random.choice(words),random.choice(words),random.randrange(0,9),random.choice(words)))
         if len(guess) >= 13 and len(guess) <=22:
             print(guess)
         else:
@@ -29,11 +29,11 @@ def run():
 
 parser = argparse.ArgumentParser(description='generate verizon passwords for cracking')
 parser.add_argument('--run', required=False, action='store_true', dest='run', help='run after sorted wordlist into verbs and nouns')
-parser.add_argument('--expand', required=False, action='store_true', dest='expand', help='expand wordlist (words.txt) into nouns.txt and verbs.txt')
+#parser.add_argument('--expand', required=False, action='store_true', dest='expand', help='expand wordlist (words.txt) into nouns.txt and verbs.txt')
 args = parser.parse_args()
 if args.run:
     run()
-elif args.expand:
-    expand()
+#elif args.expand:
+#    expand()
 else:
     parser.print_help()
